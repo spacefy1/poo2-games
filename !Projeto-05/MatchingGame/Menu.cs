89080681311a25658d20a03fc3e48c9e2af5e180
spacefy1 <161043737+spacefy1@users.noreply.github.com>
2025-09-05 09:12:01 -0300

    Add files via upload

---
+++ !Projeto-05/MatchingGame/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Menu : Form
+    {
+        private frmMain gameForm;
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameForm = new frmMain();
+            gameForm.ShowDialog(); // Use ShowDialog() pois frmMain não herda de Form
+            this.Close();
+        }
+    }
+}
